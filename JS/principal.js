d8aaad0191dc98c70d461d1540a961a14538f057
maxi7338 <leperamaxi@hotmail.com>
2023-02-19 23:36:29 -0300

    primer commit

---
+++ JS/principal.js
@@ -0,0 +1,13 @@
+ 
+    parent.addEventListener('mouseenter'), function(apretar)  
+    {parent.style.background = 'blue'
+}
+  
+
+color = document.getElementById("boton").innerHTML;
+if (color == "transparent"){
+    document.getElementById("boton").style.background-color = "#0000FF";
+}
+if (color == "negro"){
+    document.getElementById("boton").style.background-color = "#FF0000";
+}
